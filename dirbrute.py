8becce0a56faa391f248ce86b5a0af793a00a87f
wannacry3030 <wannacry.bat@proton.me>
2023-01-12 16:30:31 -0300

    att

--- dirbrute.py
+++ dirbrute.py
@@ -1,4 +1,25 @@
+import sys
 import requests
+ # USAGE: ~$ python dirbrute.py http://google.com  /usr/share/wordlists/wfuzz/general/common.txt 
  
-response = requests.get("http://g1.com.br")
-print(response.status_code)
+ 
+def brute(url, wordlist):
+    for word in wordlist:
+        try:
+            url_final = "{}/{}".format(url, word.strip())
+            response = requests.get(url_final)
+            code = response.status_code
+            if code != 404:
+                print("{} -- {}".format(url_final, code))
+        except KeyboardInterrupt:
+            sys.exit(0)
+        except:
+            pass
+             
+            
+if __name__ == "__main__":
+    url = sys.argv[1]
+    wordlist = sys.argv[2]
+    with open (wordlist, "r") as file:
+        wordlist = file.readlines()
+        brute(url, wordlist)
